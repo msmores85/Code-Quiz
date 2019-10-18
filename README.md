1ff637fc61741745973b21a8e4ced1d0aacedd35
Maria Simoes <msmores85@gmail.com>
2019-10-18 12:35:38 -0400

    Timer variables and function included in div

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Code-Quiz
 This repository provides a coding quiz site using Web APIs. 
+
+Overall Purpose: As a coding bootcamp student I want to take a timed quiz on JavaScript fundamentals that stores high scores so that I can gauge my progress compared to my peers.
+
+HTML Document: 
+
+CSS Document: 
+
+JS Document: 
--- assets/questions.js
+++ assets/questions.js
@@ -1,3 +1,4 @@
+//The following are the questions that will be a part of the quiz in the new appended div container. 
 var questions = [
     {
         title: "Commonly used data types DO NOT include:", 
@@ -26,4 +27,37 @@ var questions = [
     }
 
 ];
+
+
+//Variables: 
+
+//Functions: 
+
+//The following are variables for the timer: 
+var countdown = document.getElementById("timer");
+var startButton = document.getElementById("start");
+var seconds = 75;
+var endTime = 0;
+
+//The .addEventListener function causes the application to listen for the button to click.
+//When the button is clicked, the timer begins to countdown from 75 seconds until it reaches 0.
+startButton.addEventListener("click", function(){
+    var myCount = 0;
+    if(myCount === 0){
+        myCount = setInterval(function(){
+        
+            seconds--;
+            countdown.textContent = "Time: " + seconds + "seconds";
+        
+            if(seconds <= 0) {
+                clearInterval(myCount);
+            };
+        //}) console.log(seconds);
+    },1000);
+}
+});
+
+
+
+
 
