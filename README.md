73029105af10876ff73b5dec27120a61f95680b4
Maria Simoes <msmores85@gmail.com>
2019-10-20 19:12:23 -0400

    Updated READMe file, Event Listeners from main div, Appended Q&As, Highscores back button functionality

--- README.md
+++ README.md
@@ -3,8 +3,13 @@ This repository provides a coding quiz site using Web APIs.
 
 Overall Purpose: As a coding bootcamp student I want to take a timed quiz on JavaScript fundamentals that stores high scores so that I can gauge my progress compared to my peers.
 
-HTML Document: 
+HTML Documents: 
+    The index.html is the main page in which users can access the directions for the quiz and click the start button. From there, the user is directed to a new div that provides the first question. When an answer is clicked, the user is told if he/she is correct or incorrect and the application moves them onto the next div with the next question. When finished, users are able to add their scores and their initials to the highscores page. 
+
+    The highscores.html allows users to see their highscores which are retained by local storage. 
 
 CSS Document: 
+    The style.css document provides the styling of the page. It primarily includes the styling for the navigation bar, the buttons, the divs, and provides the responsive components of the page. 
 
 JS Document: 
+    The questions.js document provides variables and functions for the interactive elements on the page, from the changing divs to the input to local storage. The quiz questions and answers have been stored here in objects and arrays, which are then rendered onto the page through the functions of this document. The timer and its functions are also housed here. 
--- assets/questions.js
+++ assets/questions.js
@@ -1,3 +1,6 @@
+//IndexPage: 
+
+//Questions:
 //The following are the questions that will be a part of the quiz in the new appended div container. 
 var questions = [
     {
@@ -29,8 +32,6 @@ var questions = [
 ];
 
 
-//Variables: 
-
 //Functions: 
 
 //The following are variables for the timer: 
@@ -58,6 +59,62 @@ startButton.addEventListener("click", function(){
 });
 
 
+//The following are additional variables to hide elements on the page:
+var startPage = document.querySelector("#main");
 
 
 
+//The following are functions to render the questions on the page: 
+//The following function hides the main div created  the style.display = "none" component from: https://www.geeksforgeeks.org/hide-or-show-elements-in-html-using-display-property/
+startButton.addEventListener("click", function(){
+    startPage.style.display = "none";
+})
+
+
+//The following code is to render the question answers on the page:
+var question1Div = document.querySelector("#questionPage1");
+
+startButton.addEventListener("click", function(){
+    //Question 1 displayed: 
+    question1Div.style.display = "block";
+    var question1 = questions[0].title;
+    $("#question1").append(question1);
+    var list1 = document.querySelector("#list1");
+    
+    //Question1 Answers displayed:
+    var qa1 = questions[0].choices[0]; 
+    $("#q1a1").append(qa1);
+    var qa2 = questions[0].choices[1];
+    $("#q1a2").append(qa2);
+    var qa3 = questions[0].choices[2];
+    $("#q1a3").append(qa3);
+    var qa4 = questions[0].choices[3]; 
+    $("#q1a4").append(qa4);
+
+})
+
+var answer1 = questions[0].answer;
+var numberClicked = "";
+
+numberClicked.addEventListener("click", function(){
+    //Answer: 
+    if(numberClicked === answer1) {
+        var correct = document.createElement("p"); 
+        question1Div.append("Correct");
+    }else{
+        var incorrect = document.createElement("p");
+        question1Div.append("Incorrect");
+    }
+})
+
+//Incorrect answers - subtracted time from timer.
+//Quiz stops when all answers answered or timer reaches 0.
+//Addition of points to local storage
+//Display for overall points
+//Textbox for the user to type initials into
+
+
+//HighScores Page: 
+//Using local storage to house the user's highscores
+//Rendering highscores from local storage onto the Highscores page
+//Clearing the highscores with a button click
